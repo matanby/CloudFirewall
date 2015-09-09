b5d7e51ef9c7fddce8b8286236ae3c0bafc3518f
Matan Ben-Yosef <matan.ben.yosef@gmail.com>
2015-09-09 23:12:59 +0300

    Moved admin username and password to the config file.

--- cloudfirewall/config.py
+++ cloudfirewall/config.py
@@ -1,1 +1,3 @@
 SECRET_KEY = 'you-will-never-guess'
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'admin'
--- cloudfirewall/forms.py
+++ cloudfirewall/forms.py
@@ -1,29 +1,30 @@
 from wtforms import Form, StringField, PasswordField, validators
 from models import User
+import config
 
 
 class LoginForm(Form):
-    username = StringField('Username', [validators.DataRequired()])
-    password = PasswordField('Password', [validators.DataRequired()])
+	username = StringField('Username', [validators.DataRequired()])
+	password = PasswordField('Password', [validators.DataRequired()])
 
-    def __init__(self, *args, **kwargs):
-        Form.__init__(self, *args, **kwargs)
-        self.user = None
+	def __init__(self, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+		self.user = None
 
-    def validate(self):
-        rv = Form.validate(self)
-        if not rv:
-            return False
+	def validate(self):
+		rv = Form.validate(self)
+		if not rv:
+			return False
 
-        user = User(self.username.data)
+		user = User(self.username.data)
 
-        if user.username != "admin":
-            self.username.errors.append('Unknown username')
-            return False
+		if user.username != config.ADMIN_USERNAME:
+			self.username.errors.append('Invalid username')
+			return False
 
-        if self.password.data != "admin":
-            self.password.errors.append('Invalid password')
-            return False
+		if self.password.data != config.ADMIN_PASSWORD:
+			self.password.errors.append('Invalid password')
+			return False
 
-        self.user = user
-        return True
+		self.user = user
+		return True
